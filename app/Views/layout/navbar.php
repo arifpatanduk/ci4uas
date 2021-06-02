47273b6a47407a4201b808010f2b632973f829fb
Arif Wiranata Patanduk <arifp@student.uns.ac.id>
2021-06-02 13:21:38 +0700

    update register, login, set profile

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -78,15 +78,8 @@
                     <div class="d-sm-none d-lg-inline-block"><?= user()->username; ?></div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
-                    <div class="dropdown-title">Logged in 5 min ago</div>
-                    <a href="features-profile.html" class="dropdown-item has-icon">
+                    <a href="<?= base_url('/user/profile'); ?>" class="dropdown-item has-icon">
                         <i class="far fa-user"></i> Profile
-                    </a>
-                    <a href="features-activities.html" class="dropdown-item has-icon">
-                        <i class="fas fa-bolt"></i> Activities
-                    </a>
-                    <a href="features-settings.html" class="dropdown-item has-icon">
-                        <i class="fas fa-cog"></i> Settings
                     </a>
                     <div class="dropdown-divider"></div>
                     <a href="<?= base_url('logout'); ?>" class="dropdown-item has-icon text-danger">
