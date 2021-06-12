ba1fc34d7d1d1f9f92f80ac4a882845bed6d5986
Arif Wiranata Patanduk <arifp@student.uns.ac.id>
2021-06-12 14:02:02 +0700

    update sidebar

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -34,23 +34,11 @@
                 <li><a class="nav-link" href="<?= base_url('admin/users'); ?>"><i class="fas fa-users"></i> <span>Users</span></a></li>
             <?php endif; ?>
 
-            <!-- logout / Login-->
-            <?php if (logged_in()) : ?>
-                <li><a class="nav-link" href="<?= base_url('logout'); ?>"><i class="fas fa-sign-out-alt"></i> <span>Logout</span></a></li>
-            <?php else : ?>
-                <div class="mb-4 p-3 hide-sidebar-mini">
-                    <a href="<?= base_url('login'); ?>" class="btn btn-primary btn-lg btn-block btn-icon-split font-weight-bold">
-                        <i class="fas fa-rocket"></i> Join Now
-                    </a>
-                </div>
 
-            <?php endif; ?>
 
             <?php if (in_groups('admin')) : ?>
                 <li class="menu-header">Dokumen</li>
-                <li class="nav-item dropdown <?php if ($active == "dokumen") {
-                                                    echo "active";
-                                                } ?>">
+                <li class="nav-item dropdown <?= ($active === 'dokumen') ? 'active' : ''; ?>">
                     <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-book"></i> <span>Dokumen</span></a>
                     <ul class="dropdown-menu">
                         <li <?php if ($active == "dokumen") {
@@ -60,6 +48,17 @@
                         <li><a class="nav-link" href="#"><span>Sub Kategori</span></a></li>
                     </ul>
                 </li>
+            <?php endif; ?>
+
+            <!-- logout / Login-->
+            <?php if (logged_in()) : ?>
+                <li><a class="nav-link" href="<?= base_url('logout'); ?>"><i class="fas fa-sign-out-alt"></i> <span>Logout</span></a></li>
+            <?php else : ?>
+                <div class="mb-4 p-3 hide-sidebar-mini">
+                    <a href="<?= base_url('login'); ?>" class="btn btn-primary btn-lg btn-block btn-icon-split font-weight-bold">
+                        <i class="fas fa-rocket"></i> Join Now
+                    </a>
+                </div>
             <?php endif; ?>
 
         </ul>
