4f97c088d1d913eb51824e4cb6f89cc67946c9c1
arifpatanduk <arifpatanduk2@gmail.com>
2021-06-14 11:53:16 +0700

    add view & controller infografis

---
+++ app/Views/admin/infografis.php
@@ -0,0 +1,72 @@
+<?= $this->extend('layout/main'); ?>
+
+<?= $this->section('content'); ?>
+<!-- Main Content -->
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1>Document</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="<?= base_url(); ?>">Home</a></div>
+                <div class="breadcrumb-item">Document</div>
+            </div>
+        </div>
+
+        <div class="section-body">
+            <h2 class="section-title">Chart.js</h2>
+            <p class="section-lead">
+                We use 'Chart.JS' made by @chartjs. You can check the full documentation <a href="http://www.chartjs.org/">here</a>.
+            </p>
+
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Line Chart</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="myChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Bar Chart</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="myChart2"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Doughnut Chart</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="myChart3"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Pie Chart</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="myChart4"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+</div>
+
+<?= $this->endSection(); ?>
